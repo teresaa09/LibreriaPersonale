34b0f299ee27b664cfc570a6bec04f93628825b6
teresaa09 <121051871+teresaa09@users.noreply.github.com>
2025-08-17 18:59:47 +0200

    classe libro

--- src/libreria/Libro.java
+++
@@ -1,4 +0,0 @@
-package libreria;
-
-public class Libro {
-}
---
+++ src/main/java/libreria/service/Libro.java
@@ -0,0 +1,74 @@
+package main.java.libreria.service;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Libro {
+    private String titolo;
+    private String autore;
+    private String codiceISBN;
+    private Generi genere;
+    private int valutazione;
+    private StatoDellaLettura status;
+
+    // Costruttore per JSON
+    public Libro(@JsonProperty("titolo") String titolo,
+                 @JsonProperty("autore") String autore,
+                 @JsonProperty("codiceISBN") String codiceISBN,
+                 @JsonProperty("genere") Generi genere,
+                 @JsonProperty("valutazione") int valutazione,
+                 @JsonProperty("status") StatoDellaLettura status) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.codiceISBN = codiceISBN;
+        this.genere = genere;
+        this.valutazione = valutazione;
+        this.status = status;
+    }
+
+    // Costruttore vuoto
+    public Libro() {
+        this.genere = Generi.ROMANZO;
+        this.status = StatoDellaLettura.DA_LEGGERE;
+        this.valutazione = 0;
+    }
+
+    // Costruttore semplificato
+    public Libro(String titolo, String autore) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.genere = Generi.ROMANZO;  // Default
+        this.status = StatoDellaLettura.DA_LEGGERE;  // Default
+        this.valutazione = 0;
+    }
+
+    // GETTER e SETTER
+    public String getTitolo() { return titolo; }
+    public void setTitolo(String titolo) { this.titolo = titolo; }
+
+    public String getAutore() { return autore; }
+    public void setAutore(String autore) { this.autore = autore; }
+
+    public String getCodiceISBN() { return codiceISBN; }
+    public void setCodiceISBN(String codiceISBN) { this.codiceISBN = codiceISBN; }
+
+    public Generi getGenere() { return genere; }
+    public void setGenere(Generi genere) { this.genere = genere; }
+
+    public int getValutazione() { return valutazione; }
+    public void setValutazione(int valutazione) {
+        if (valutazione < 0 || valutazione > 5) {
+            throw new IllegalArgumentException("La valutazione deve essere tra 0 e 5");
+        }
+        this.valutazione = valutazione;
+    }
+
+    public StatoDellaLettura getStatus() { return status; }
+    public void setStatus(StatoDellaLettura status) { this.status = status; }
+
+    @Override
+    public String toString() {
+        return titolo + " di " + autore + " [" + genere + "] - Valutazione: " + valutazione;
+    }
+}
+
+
